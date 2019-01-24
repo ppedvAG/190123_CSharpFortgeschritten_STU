f7f09d90d8672c85270ccd59caafa35b6553d232
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-01-24 11:17:27 +0100

    V1 von SingleResponsibility

--- SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
+++ SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
@@ -8,26 +8,64 @@ namespace SOLID_Taschenrechner
 {
     class Program
     {
+        // Bootstrapping -> Initialisieren der Logik
         static void Main(string[] args)
         {
-            Console.WriteLine("Bitte geben Sie die Rechenformel ein:");
+            new KonsolenUI().Start();
+        }
+    }
 
-            string formel = Console.ReadLine(); // "2 + 2"
+    class Formel
+    {
+        public int Operand1 { get; set; }
+        public int Operand2 { get; set; }
+        public char Operator { get; set; }
+    }
 
-            string[] teile = formel.Split(' ');
+    class StringSplitParser
+    {
+        public Formel Parse(string input)
+        {
+            string[] teile = input.Split(' ');
             int zahl1 = Convert.ToInt32(teile[0]);
             int zahl2 = Convert.ToInt32(teile[2]);
 
-            int ergebnis = 0;
-            if (teile[1] == "+")
-                ergebnis = zahl1 + zahl2;
-            else if (teile[1] == "-")
-                ergebnis = zahl1 - zahl2;
-                ; //....
+            return new Formel { Operand1 = zahl1, Operand2 = zahl2, Operator = teile[1][0] };
+        }
+    }
 
+    class Rechner
+    {
+        public int Berechne(Formel formel)
+        {
+            if (formel.Operator == '+')
+                return formel.Operand1 + formel.Operand2;
+            else if (formel.Operator == '-')
+                return formel.Operand1 + formel.Operand2;
+            
+            throw new NotSupportedException("Operator ist unbekannt");
+        }
+    }
+
+    class KonsolenUI
+    {
+        public void Start()
+        {
+            // UI
+            Console.WriteLine("Bitte geben Sie die Rechenformel ein:");
+            string input = Console.ReadLine(); // "2 + 2"
+
+            // Parser
+            StringSplitParser parser = new StringSplitParser();
+            Formel f = parser.Parse(input);
+
+            // Rechner
+            Rechner r = new Rechner();
+            int ergebnis = r.Berechne(f);
+
+            // UI
             // C# 6 String-Interpolation
             Console.WriteLine($"Das Ergebnis ist: {ergebnis}");
-
             Console.WriteLine("---ENDE--");
             Console.ReadKey();
         }
