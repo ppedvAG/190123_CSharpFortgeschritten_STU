65c9357f7bd7c0a39152bb72f4ff648c997ede3c
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-01-24 16:34:21 +0100

    Pay2Win - System implementiert

--- SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
+++ SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
@@ -1,7 +1,10 @@
-﻿using SOLID_Taschenrechner.FreeFeatures;
+﻿using SOLID_Taschenrechner.Domain;
 using SOLID_Taschenrechner.Logic;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,7 +16,32 @@ namespace SOLID_Taschenrechner
         static void Main(string[] args)
         {
             var parser = new RegexParser();
-            var rechner = new ModularerRechner(new Addition(),new Subtraktion());
+
+            string pay2winDirectory = Path.Combine(Environment.CurrentDirectory, "Pay2Win");
+            if(Directory.Exists(pay2winDirectory))
+            {
+                foreach (string file in Directory.GetFiles(pay2winDirectory))
+                {
+                    if (Path.GetExtension(file) == ".dll" ||
+                        Path.GetExtension(file) == ".exe")
+                        Assembly.LoadFrom(file);
+                }
+            }
+
+            var alleDatentypenDieIRechenoperationImplementieren =
+                AppDomain.CurrentDomain.GetAssemblies()
+                                       .SelectMany(assembly => assembly.GetTypes())
+                                       .Where(type => typeof(IRechenoperation).IsAssignableFrom(type) &&
+                                                      type.IsInterface == false &&
+                                                      type.IsAbstract == false)
+                                       .ToArray();
+
+            IRechenoperation[] alleOperationen = alleDatentypenDieIRechenoperationImplementieren
+                                                 .Select(x => (IRechenoperation)Activator.CreateInstance(x))
+                                                 .ToArray();
+
+            var rechner = new ModularerRechner(alleOperationen);
+            // var rechner = new ModularerRechner(new Addition(),new Subtraktion());
             new KonsolenUI(parser,rechner).Start();
         }
     }
