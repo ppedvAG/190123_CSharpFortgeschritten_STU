1d0c1093d4a94bcdea7289ac098d4ecf3c5f8f55
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-01-24 11:03:18 +0100

    Basissituation: kein SOLID

--- SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
+++ SOLID_Taschenrechner/SOLID_Taschenrechner/Program.cs
@@ -10,6 +10,26 @@ namespace SOLID_Taschenrechner
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Bitte geben Sie die Rechenformel ein:");
+
+            string formel = Console.ReadLine(); // "2 + 2"
+
+            string[] teile = formel.Split(' ');
+            int zahl1 = Convert.ToInt32(teile[0]);
+            int zahl2 = Convert.ToInt32(teile[2]);
+
+            int ergebnis = 0;
+            if (teile[1] == "+")
+                ergebnis = zahl1 + zahl2;
+            else if (teile[1] == "-")
+                ergebnis = zahl1 - zahl2;
+                ; //....
+
+            // C# 6 String-Interpolation
+            Console.WriteLine($"Das Ergebnis ist: {ergebnis}");
+
+            Console.WriteLine("---ENDE--");
+            Console.ReadKey();
         }
     }
 }
