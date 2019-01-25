5b91a1150c990f74390252777eb631c7453cec14
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-01-25 11:51:46 +0100

    Asynchrones Programmieren variante .NET Framework 4.0 mit

---
+++ AsyncUndAwait/AsyncUndAwait/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsyncUndAwait
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ProgressBarFertigEvent += IchHabeFertig;
+        }
+
+        private void IchHabeFertig(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ende");
+        }
+
+        public event EventHandler ProgressBarFertigEvent;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Start");
+            MachEtwas();
+        }
+
+        private void MachEtwas()
+        {
+            Task t1 = Task.Run(() =>
+            {
+                for (int i = 0; i <= 100; i++)
+                {
+                    // "UI-Thread, bitte führe für mich diesen code aus:"
+                    Dispatcher.Invoke(() => progressBarFortschritt.Value = i);
+                    Thread.Sleep(100);
+                }
+                ProgressBarFertigEvent?.Invoke(this, EventArgs.Empty);
+            });
+        }
+    }
+}
