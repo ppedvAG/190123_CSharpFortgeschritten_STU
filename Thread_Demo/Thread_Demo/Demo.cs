517754e0eba76a71e60310e7a98a23ff1db93a0e
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-01-25 10:16:13 +0100

    Threads

---
+++ Thread_Demo/Thread_Demo/Demo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Thread_Demo
+{
+    class Demo
+    {
+        private object syncObject = new object();
+        private int wert = default; // also 0
+        public void MachWas()
+        {
+            while (true)
+            {
+                // Monitor.Enter(syncObject); // Nur 1 Thread darf da rein
+                lock (syncObject)
+                {
+                    wert++;
+                    if (wert > 100)
+                        break;
+                    Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: {wert}");
+                }
+                // Monitor.Exit(syncObject);
+
+                // Monitor:     Nur 1 darf den Codeblock ausführen
+                // Mutex:       Nur 1 (von 2..N instanzen) darf hinein (Prozessübergreifender Monitor-Block)
+                // Semaphore:   Mehrere Applikationen auf einem Codeblock
+
+                // Threadsichere Operationen
+                // Interlocked ... z.B. Increment, .Decrement(ref wert);
+            }
+        }
+    }
+}
---
+++ Thread_Demo/Thread_Demo/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Thread_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Thread - Basics
+            //Thread t1 = new Thread(Punkterl);
+            //Thread t2 = new Thread(Stricherl);
+
+            //t1.IsBackground = true;
+            //t2.IsBackground = true;
+            //t1.Start();
+            //t2.Start();
+
+            //Thread.Sleep(3000);
+            //t1.Abort();
+            //t1.Join(); // Warten, bis der Thread fertig ist 
+            #endregion
+
+            Demo d1 = new Demo();
+
+            Thread t1 = new Thread(d1.MachWas);
+            Thread t2 = new Thread(d1.MachWas);
+
+            t1.Start();
+            t2.Start();
+
+            // Thread.Sleep(1000); // Suspended, andere Threads kommen drann
+            // Thread.Sleep(0);    // Gibt die restliche übrige CPU-Zeit auf und ein anderer Thread kommt sofort drann
+            // Thread.SpinWait();  // "Bullshit-Berechnung" um Zeit X zu "warten"
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
+        }
+
+        private static void Stricherl()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Thread.Sleep(100);
+                Console.Write('-');
+            }
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId); 
+        }
+        private static void Punkterl()
+        {
+            try
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    Thread.Sleep(100);
+                    Console.Write('.');
+                }
+            }
+            catch (ThreadAbortException)
+            {
+                Console.WriteLine("Thread wird beendet ....");
+                Thread.Sleep(4000);
+                return;
+            }
+        }
+    }
+}
