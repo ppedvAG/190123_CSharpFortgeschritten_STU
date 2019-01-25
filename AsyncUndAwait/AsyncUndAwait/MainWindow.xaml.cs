907dc87c237fbadb0cfcb676bcbfe4ee2da40c25
Michael Zöhling <Michael.Zoehling@outlook.com>
2019-01-25 12:12:52 +0100

    Async-Await

--- AsyncUndAwait/AsyncUndAwait/MainWindow.xaml.cs
+++ AsyncUndAwait/AsyncUndAwait/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -24,25 +25,31 @@ namespace AsyncUndAwait
         public MainWindow()
         {
             InitializeComponent();
-            ProgressBarFertigEvent += IchHabeFertig;
         }
 
-        private void IchHabeFertig(object sender, EventArgs e)
-        {
-            MessageBox.Show("Ende");
-        }
+        // async void => NICHT VERWENDEN, nur in EventHandler (wo es nicht anders geht)
+        // -> Expeption können nicht weitergegeben werden, wenn es keine Rückgabe gibt
 
-        public event EventHandler ProgressBarFertigEvent;
+        // async Task => IMMER VERWENDEN (auch bei Methoden, die normalerweise void zurückgeben)
+        // async Task<T> => IMMER verwenden, wenn es eine Rückgabe gibt
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void Button_Click(object sender, RoutedEventArgs e)
         {
+            GibUhrzeit();
+
             MessageBox.Show("Start");
-            MachEtwas();
+            await  MachEtwas(); // Wartet hier wie Task.WaitAll(), ABER der UI-Thread kann währenddessen weitermachen und blockiert NICHT
+            MessageBox.Show("Ende");
+
+        //    StreamReader sr = new StreamReader("demo.txt");
+        //    string zeile = sr.ReadLine(); // Synchron - Blockiert
+        //    zeile = sr.ReadLineAsync().Result; // Asynchrone logik synchron aufrufen - Blockiert
+        //    zeile = await sr.ReadLineAsync(); // Asynchron - Blockiert nicht
         }
 
-        private void MachEtwas()
+        private Task MachEtwas()
         {
-            Task t1 = Task.Run(() =>
+            return Task.Run(() =>
             {
                 for (int i = 0; i <= 100; i++)
                 {
@@ -50,7 +57,13 @@ namespace AsyncUndAwait
                     Dispatcher.Invoke(() => progressBarFortschritt.Value = i);
                     Thread.Sleep(100);
                 }
-                ProgressBarFertigEvent?.Invoke(this, EventArgs.Empty);
+            });
+        }
+
+        private async void GibUhrzeit()
+        {
+            Task.Run(() => {
+                throw new FormatException();
             });
         }
     }
